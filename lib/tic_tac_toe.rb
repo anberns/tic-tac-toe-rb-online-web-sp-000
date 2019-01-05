de21c69ef20be51ca85d75c8db72e44e4cbf97e8
Aaron Berns <anberns@gmail.com>
2019-01-05 20:51:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,6 +10,57 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+# display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# input_to_index
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+# move
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+#position_taken?
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+# valid_move?
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+# turn
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  turns = 0 
+  while turns < 9 
+    turn(board)
+    turns += 1
+  end
+end
+
 # won?
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
